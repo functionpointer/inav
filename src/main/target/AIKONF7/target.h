2e7e023802ade9537f41e7be8e2e34a1f7390217
functionpointer <suspendfunction@gmail.com>
2020-07-03 13:44:54 +0200

    WIP AikonF7 target

--- src/main/target/AIKONF7/target.h
+++ src/main/target/AIKONF7/target.h
@@ -21,56 +21,68 @@
 #define TARGET_BOARD_IDENTIFIER "AIK7"
 #define USBD_PRODUCT_STRING  "AIKONF7"
 
-#define LED0                    PA14  //Blue   SWCLK
-#define LED1                    PA13  //Green  SWDIO
+#define LED0                    PC13
+#define BEEPER                  PC15
+//#define BEEPER_INVERTED
 
-#define BEEPER                  PC13
-#define BEEPER_INVERTED
-
-// *************** SPI1 Gyro & ACC *******************
-#define USE_TARGET_IMU_HARDWARE_DESCRIPTORS
-
-#define USE_SPI
-#define USE_SPI_DEVICE_1
-#define SPI1_SCK_PIN            PA5
-#define SPI1_MISO_PIN           PA6
-#define SPI1_MOSI_PIN           PA7
-
-#define USE_DUAL_GYRO
-
-#define USE_IMU_MPU6000
-#define IMU_MPU6000_ALIGN       CW180_DEG_FLIP
-#define MPU6000_CS_PIN          PB2
-#define MPU6000_SPI_BUS         BUS_SPI1
-#define MPU6000_EXTI_PIN        PC4
-
-#define USE_IMU_MPU6500
-#define IMU_MPU6500_ALIGN       CW90_DEG
-#define MPU6500_CS_PIN          PC15
-#define MPU6500_SPI_BUS         BUS_SPI1
-#define MPU6500_EXTI_PIN        PC3
-
-
-
+// MPU6000 interrupts
 #define USE_EXTI
+#define GYRO_INT_EXTI            PC4
 #define USE_MPU_DATA_READY_SIGNAL
 
-// *************** I2C /Baro/Mag *********************
-#define USE_I2C
-#define USE_I2C_DEVICE_1
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
+#define USE_IMU_MPU6000
+#define IMU_MPU6000_ALIGN       CW0_DEG
+#define MPU6000_CS_PIN          SPI1_NSS_PIN
+#define MPU6000_SPI_BUS         BUS_SPI1
 
-// External I2C BARO
+//#define USE_IMU_MPU6500
+//#define IMU_MPU6500_ALIGN       CW0_DEG
+//#define MPU6500_CS_PIN          SPI1_NSS_PIN
+//#define MPU6500_SPI_BUS         BUS_SPI1
+
+#define SERIAL_PORT_COUNT       6   //VCP, UART1, UART2, UART3, UART4, UART5
+
+#define USE_VCP
+//#define VBUS_SENSING_PIN        PD2
+//#define VBUS_SENSING_ENABLED
+
+#define USE_UART1
+#define UART1_RX_PIN            PA10
+#define UART1_TX_PIN            PA9
+
+#define USE_UART2
+#define UART2_TX_PIN            PA2
+#define UART2_RX_PIN            PA3
+
+#define USE_UART3
+#define UART3_RX_PIN            PC11
+#define UART3_TX_PIN            PC10
+
+#define USE_UART4
+#define UART4_TX_PIN            PA0
+#define UART4_RX_PIN            PA01
+
+#define USE_UART5
+#define UART5_TX_PIN            PC12
+#define UART5_RX_PIN            PD02
+
+#define USE_I2C
+#define USE_I2C_DEVICE_2
+#define I2C2_SCL                PB10
+#define I2C2_SDA                PB11
+
+#define DEFAULT_I2C_BUS         BUS_I2C2
+#define EXTERNAL_I2C_BUS        DEFAULT_I2C_BUS
 
 #define USE_BARO
-#define BARO_I2C_BUS I2C_EXT_BUS
+#define BARO_I2C_BUS            EXTERNAL_I2C_BUS
+#define USE_BARO_BMP280
 #define USE_BARO_MS5611
 #define USE_BARO_BMP085
-#define USE_BARO_BMP280
 
 #define USE_MAG
-#define MAG_I2C_BUS             BUS_I2C1
+#define MAG_I2C_BUS             EXTERNAL_I2C_BUS
+#define USE_MAG_AK8963
 #define USE_MAG_AK8975
 #define USE_MAG_HMC5883
 #define USE_MAG_QMC5883
@@ -79,115 +91,62 @@
 #define USE_MAG_MAG3110
 #define USE_MAG_LIS3MDL
 
-#define TEMPERATURE_I2C_BUS     BUS_I2C1
+#define RANGEFINDER_I2C_BUS     EXTERNAL_I2C_BUS
+#define TEMPERATURE_I2C_BUS     EXTERNAL_I2C_BUS
+#define PITOT_I2C_BUS           EXTERNAL_I2C_BUS
+#define PCA9685_I2C_BUS         EXTERNAL_I2C_BUS
 
-#define PITOT_I2C_BUS           BUS_I2C1
+#define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
+#define SERIALRX_PROVIDER       SERIALRX_SBUS
+#define SERIALRX_UART           SERIAL_PORT_USART1
 
-#define USE_RANGEFINDER
-#define RANGEFINDER_I2C_BUS     BUS_I2C1
+#define DEFAULT_FEATURES        (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_BLACKBOX)
 
-// *************** SPI2 OSD ***********************
+#define USE_ADC
+#define ADC_CHANNEL_1_PIN               PC1
+#define ADC_CHANNEL_2_PIN               PC2
+#define ADC_CHANNEL_3_PIN               PC3
+
+#define CURRENT_METER_ADC_CHANNEL       ADC_CHN_1
+#define VBAT_ADC_CHANNEL                ADC_CHN_2
+#define RSSI_ADC_CHANNEL                ADC_CHN_3
+
+#define USE_SPI
+#define USE_SPI_DEVICE_1
+#define SPI1_NSS_PIN            PA4
+#define SPI1_SCK_PIN            PA5
+#define SPI1_MISO_PIN           PA6
+#define SPI1_MOSI_PIN           PA7
+
 #define USE_SPI_DEVICE_2
+#define SPI2_NSS_PIN            PB12
 #define SPI2_SCK_PIN            PB13
 #define SPI2_MISO_PIN           PB14
 #define SPI2_MOSI_PIN           PB15
 
-#define USE_OSD
-#define USE_MAX7456
-#define MAX7456_SPI_BUS         BUS_SPI2
-#define MAX7456_CS_PIN          PB12
-
-// *************** SPI3 SD BLACKBOX*******************
 #define USE_SPI_DEVICE_3
+#define SPI3_NSS_PIN            PA15
 #define SPI3_SCK_PIN            PC10
 #define SPI3_MISO_PIN           PC11
 #define SPI3_MOSI_PIN           PC12
 
-#if defined(MATEKF722MINI)
-#   define USE_FLASHFS
-#   define USE_FLASH_M25P16
-#   define M25P16_SPI_BUS          BUS_SPI3
-#   define M25P16_CS_PIN           PD2
-#   define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
-#else
-#   define USE_SDCARD
-#   define USE_SDCARD_SPI
-#   define SDCARD_SPI_BUS          BUS_SPI3
-#   define SDCARD_CS_PIN           PD2
-#   define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
-#endif
+#define MAX_PWM_OUTPUT_PORTS    6
 
-// *************** UART *****************************
-#define USE_VCP
-#define USB_DETECT_PIN          PC14
-#define USE_USB_DETECT
+#define TARGET_IO_PORTA         0xffff
+#define TARGET_IO_PORTB         0xffff
+#define TARGET_IO_PORTC         0xffff
+#define TARGET_IO_PORTD         0xffff
 
-#define USE_UART1
-#define UART1_TX_PIN            PA9
-#define UART1_RX_PIN            PA10
+#define USE_OSD
+#define USE_MAX7456
+#define MAX7456_SPI_BUS         BUS_SPI3
+#define MAX7456_CS_PIN          SPI3_NSS_PIN
 
-#define USE_UART2
-#define UART2_TX_PIN            PA2
-#define UART2_RX_PIN            PA3
-
-#define USE_UART3
-#define UART3_TX_PIN            PB10
-#define UART3_RX_PIN            PB11
-
-#define USE_UART4
-#define UART4_TX_PIN            PA0
-#define UART4_RX_PIN            PA1
-
-#define USE_UART6
-#define UART6_TX_PIN            PC6
-#define UART6_RX_PIN            PC7
-     
-#define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN      PA2
-#define SOFTSERIAL_1_RX_PIN      PA2
-
-#define SERIAL_PORT_COUNT       7
-
-#define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
-#define SERIALRX_PROVIDER       SERIALRX_SBUS
-#define SERIALRX_UART           SERIAL_PORT_USART2
-
-// *************** ADC *****************************
-#define USE_ADC
-#define ADC_INSTANCE                ADC1
-#define ADC1_DMA_STREAM             DMA2_Stream0
-
-#define ADC_CHANNEL_1_PIN           PC2
-#define ADC_CHANNEL_2_PIN           PC1
-#define ADC_CHANNEL_3_PIN           PC0
-#define ADC_CHANNEL_4_PIN           PA4
-
-#define VBAT_ADC_CHANNEL            ADC_CHN_1
-#define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
-#define RSSI_ADC_CHANNEL            ADC_CHN_3
-#define AIRSPEED_ADC_CHANNEL           ADC_CHN_4
-
-// *************** PINIO ***************************
-#define USE_PINIO
-#define USE_PINIOBOX
-#define PINIO1_PIN                  PC8 // VTX power switcher
-#define PINIO2_PIN                  PC9 // 2xCamera switcher
-
-// *************** LEDSTRIP ************************
 #define USE_LED_STRIP
-#define WS2811_PIN                  PA8
+#define WS2811_PIN              PB6
 
-#define DEFAULT_FEATURES                (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TX_PROF_SEL | FEATURE_BLACKBOX)
-#define CURRENT_METER_SCALE     179
-
-#define USE_SERIAL_4WAY_BLHELI_INTERFACE
-
-#define TARGET_IO_PORTA 0xffff
-#define TARGET_IO_PORTB 0xffff
-#define TARGET_IO_PORTC 0xffff
-#define TARGET_IO_PORTD 0xffff
-
-#define MAX_PWM_OUTPUT_PORTS        8
-#define USE_DSHOT
-#define USE_SERIALSHOT
-#define USE_ESC_SENSOR
+#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+#define M25P16_CS_PIN           SPI2_NSS_PIN
+#define M25P16_SPI_BUS          BUS_SPI2
+#define USE_FLASHFS
+#define USE_FLASH_M25P16
